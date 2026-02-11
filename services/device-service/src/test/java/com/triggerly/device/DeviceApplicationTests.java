c140c93aa58a16435bdc13e43a746ab9a534e676
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2026-02-11 14:12:36 +0300

    project template init

---
+++ services/device-service/src/test/java/com/triggerly/device/DeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triggerly.device;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
